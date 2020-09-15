0f4fc11cffd72c9ac3367818acaeac1434fbbc53
Michael Sliger <michaelj.sliger@gmail.com>
2020-09-15 13:03:58 -0600

    most functionality

--- server.js
+++ server.js
@@ -1,30 +1,50 @@
 /* eslint-disable indent */
+
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-
+const cors = require('cors');
+const helmet = require('helmet');
+const movieList = require('./movieList.js');
 const app = express();
-app.use(morgan('dev'));
 
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
 app.use(function validateBearerToken(req, res, next) {
 
-    const bearerToken = req.get('Authorization').split(' ')[1];
-
+    const authToken = req.get('Authorization');
     const apiToken = process.env.API_TOKEN;
-    console.log(apiToken);
-    if (bearerToken !== apiToken) {
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
         return res.status(401).json({error: 'Unauthorized request'});
     }
+
     next();
 });
 
 
 
 function handleGet(req, res) {
-    res.send('Movies??');
+    const { genre, country, avg_vote } = req.query;
+    let results = [ ...movieList ];
+    console.log(req.query);
+    if (genre) {
+       results = results.filter(el => el.genre.toLowerCase().includes(genre.toLowerCase()));
+    }
+    if (country) {
+        results = results.filter(el => el.country.toLowerCase().includes(country.toLowerCase()));
+    }
+    if (avg_vote) {
+        results = results.filter(el => el.avg_vote >= parseFloat(avg_vote));
+    }
+
+
+    res.send(results);
+
 }
 
-app.get('/movies', handleGet);
+app.get('/movie', handleGet);
 
 
 
